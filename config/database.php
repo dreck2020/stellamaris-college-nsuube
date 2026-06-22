b81b2379b616b247c4ef0be931a0cda833f0c7fe
dreck2020 <muhwezidreck12@gmail.com>
2026-06-22 04:25:32 +0300

    Add files via upload

--- config/database.php
+++ config/database.php
@@ -1,1 +1,24 @@
+<?php
+// config/database.php
+class Database {
+    private $host = "sql202.infinityfree.com";
+    private $db_name = "if0_41586402_stella_maris_db";
+    private $username = "if0_41586402";
+    private $password = "YqLsW6xicwr";
+    public $conn;
 
+    public function getConnection() {
+        $this->conn = null;
+        try {
+            $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name . ";charset=utf8mb4", 
+                                  $this->username, $this->password);
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_SILENT);
+            $this->conn->exec("set names utf8");
+        } catch(PDOException $exception) {
+            // Silent fail - no error message shown to users
+            $this->conn = null;
+        }
+        return $this->conn;
+    }
+}
+?>
